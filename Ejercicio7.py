5c752b243d3bb5b07fd6aec2728ca0d5ceac5c07
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2022-02-13 17:57:46 +0100

    mas pruebas

--- Ejercicio7.py
+++ Ejercicio7.py
@@ -1,9 +1,12 @@
 def viaje(alumnos=int(input("Cuantos alumnos van ")), dias=int(input("cuantos dias son "))):
+    coste=0
     if alumnos<=25:
-        coste=67.30*alumnos+3.5*alumnos*dias
+        coste=67.30*alumnos+3.5*alumnos*dias+4.75*alumnos*dias
+        print(coste)
     elif alumnos>25 and alumnos<=30:
         coste=61*alumnos+3.5*alumnos*dias+4.75*alumnos*dias
     elif alumnos>=31 and alumnos<=35:
         coste=61*alumnos+3.5*alumnos*dias+4*alumnos*dias
     elif alumnos>35:
         coste=61*alumnos+3.5*alumnos*dias+3.5*alumnos*dias
+    return print(coste)
